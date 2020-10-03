53eef4758e7afc001bb1586f4f67d79c79805c01
Pablo-VE <53917989+Pablo-VE@users.noreply.github.com>
2020-10-02 23:04:07 -0600

    Metodo buscar Vehiculo por estado y grupo

--- agencia/src/main/java/org/una/agencia/repositories/IVehiculoRepository.java
+++ agencia/src/main/java/org/una/agencia/repositories/IVehiculoRepository.java
@@ -5,7 +5,10 @@
  */
 package org.una.agencia.repositories;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.una.agencia.entities.Vehiculo;
 
 /**
@@ -13,5 +16,7 @@ import org.una.agencia.entities.Vehiculo;
  * @author Jeffry
  */
 public interface IVehiculoRepository extends JpaRepository<Vehiculo, Long> {
+    @Query("SELECT v FROM Vehiculo v LEFT JOIN v.grupo g WHERE g.nombre = :nombreGrupo AND v.estado = :estado")
+    public List<Vehiculo> findByNombreGrupoAndEstado(@Param("nombreGrupo") String nombreGrupo, @Param("estado")boolean estado);
     
 }
