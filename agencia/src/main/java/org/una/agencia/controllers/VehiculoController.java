c8f5963296b30133a5cd402aa65190d91d066a78
Lucho321 <54681195+Lucho321@users.noreply.github.com>
2020-10-02 22:35:19 -0600

    Controladores

---
+++ agencia/src/main/java/org/una/agencia/controllers/VehiculoController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.agencia.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.una.agencia.services.IVehiculoService;
+ 
+
+/**
+ *
+ * @author Luis
+ */
+
+@RestController
+public class VehiculoController {
+    
+    @Autowired
+    private IVehiculoService vehiculoService;
+
+    @GetMapping() 
+    public @ResponseBody
+    ResponseEntity<?> findAll() {
+        try {
+            return new ResponseEntity(vehiculoService.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getClass(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{id}") 
+    public ResponseEntity<?> findById(@PathVariable(value = "id") Long id) {
+        try {
+            return new ResponseEntity<>(vehiculoService.findById(id), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
