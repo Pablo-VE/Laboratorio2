74374432899a66784e9b0a8589f2690daf939a45
Pablo-VE <53917989+Pablo-VE@users.noreply.github.com>
2020-10-02 20:02:10 -0600

    Direccion DTO

---
+++ agencia/src/main/java/org/una/agencia/dto/DireccionDTO.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.agencia.dto;
+
+import javax.json.bind.annotation.JsonbDateFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ *
+ * @author Pablo-VE
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor 
+@ToString
+public class DireccionDTO {
+    private Long id;
+    private String calle;
+    private int numero;
+    private String codigoPostal;
+    @JsonbDateFormat(value = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private java.util.Date fechaRegistro;
+    @JsonbDateFormat(value = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private java.util.Date fechaModificacion;
+    private OficinaDTO oficina;
+    
+}
